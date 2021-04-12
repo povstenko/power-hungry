c440ee61356303d735a860d752958650619cc369
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-12 15:59:54 +0300

    pass test case #1

--- solution.py
+++ solution.py
@@ -1,2 +1,21 @@
+from functools import reduce
+
 def solution(xs):
-    # Your code here
+    xs.sort()
+    neg = [i for i in xs if i < 0]
+    pos = [i for i in xs if i >=0]
+    print(neg)
+    print(pos)
+    
+    res = 1
+    if len(neg) > 2:
+        # TODO
+        print()
+    else :
+        print(pos[-3:])
+        res = reduce((lambda x, y: x*y), pos[-3:])
+    
+    return res
+
+print(solution([2, 0, 2, 2, 0]))
+print(solution([-2, -3, 4, -5]))
