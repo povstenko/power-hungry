96218eba324cc45c2122891b8b1d1998c9945de1
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-12 16:39:09 +0300

    Submit solution

--- solution.py
+++ solution.py
@@ -1,16 +1,23 @@
 from functools import reduce
 
 def solution(xs):
-    xs.sort()
+    if len(xs) == 0:
+        return str(0)
+    elif len(xs) == 1:
+        return str(xs[0])
+
+    pos = [i for i in xs if i > 0]
     neg = [i for i in xs if i < 0]
-    pos = [i for i in xs if i >=0]
+
+    if len(neg) % 2 == 1:
+        neg.remove(max(neg))
+
+    if len(neg) == 0 and len(pos) == 0:
+        return str(0)
+
+    res = reduce((lambda x,y: x*y), pos + neg)
     
-    if len(neg) > 2:
-        res = reduce((lambda x, y: x*y), neg[:2]) * max(pos)
-    else :
-        res = reduce((lambda x, y: x*y), pos[-3:])
-    
-    return res
+    return str(res)
 
 # print(solution([2, 0, 2, 2, 0]))
 # print(solution([-2, -3, 4, -5]))
