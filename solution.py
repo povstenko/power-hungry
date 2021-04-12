32872fb18e597117e661adc29371c67f469a85c6
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-12 16:02:00 +0300

    Negative power case

--- solution.py
+++ solution.py
@@ -9,13 +9,14 @@ def solution(xs):
     
     res = 1
     if len(neg) > 2:
-        # TODO
-        print()
+        print(neg[:2])
+        print(max(pos))
+        res = reduce((lambda x, y: x*y), neg[:2]) * max(pos)
     else :
         print(pos[-3:])
         res = reduce((lambda x, y: x*y), pos[-3:])
     
     return res
 
-print(solution([2, 0, 2, 2, 0]))
+# print(solution([2, 0, 2, 2, 0]))
 print(solution([-2, -3, 4, -5]))
