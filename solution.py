9e2f5fc9bdfaf2928060350d4369943bf448cfd5
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-12 16:05:55 +0300

    Verify solution

--- solution.py
+++ solution.py
@@ -4,19 +4,13 @@ def solution(xs):
     xs.sort()
     neg = [i for i in xs if i < 0]
     pos = [i for i in xs if i >=0]
-    print(neg)
-    print(pos)
     
-    res = 1
     if len(neg) > 2:
-        print(neg[:2])
-        print(max(pos))
         res = reduce((lambda x, y: x*y), neg[:2]) * max(pos)
     else :
-        print(pos[-3:])
         res = reduce((lambda x, y: x*y), pos[-3:])
     
     return res
 
 # print(solution([2, 0, 2, 2, 0]))
-print(solution([-2, -3, 4, -5]))
+# print(solution([-2, -3, 4, -5]))
